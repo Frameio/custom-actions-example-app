75894786b880fd291a1a0e915566dfcf6179bfbe
Steve Kaliski <sjkaliski@gmail.com>
2018-12-07 17:10:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# custom-actions-example-app
+
+This is an example application which shows how to build a Custom Action for Frame.io. In this example, we'll present the user with a form asking them to provide their name and favorite color. On submission, we'll present the user with a confirmation message.
+
+For more information about how Custom Actions work, check out our [documentation](https://docs.frame.io/docs/custom-actions).
+
+## Setup
+
+```
+$ npm install
+```
+
+## Usage
+
+```
+$ node index.js
+```
+
+By default the application runs on port 3000, however this can be overrided by setting the `PORT` environment variable.
+
+The service will need to run at a publically accessible address. For quick testing, you can use [ngrok](https://ngrok.com/) or similar service.
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  if (req.body.data) {
+    const { name, color } = req.body.data;
+
+    res.json({
+      title: '🎉 Success!',
+      description: `Hi there ${name}! Your favorite color is ${color}.`,
+    });
+    return;
+  }
+
+  res.json({
+    title: 'Hello World!',
+    description: 'Tell me about yourself!',
+    fields: [
+      {
+        type: 'text',
+        name: 'name',
+        label: 'Name',
+      },
+      {
+        type: 'select',
+        name: 'color',
+        label: 'Favorite Color',
+        options: [
+          {
+            name: 'Blue',
+            value: 'blue',
+          },
+          {
+            name: 'Red',
+            value: 'blue',
+          },
+        ],
+      },
+    ],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
